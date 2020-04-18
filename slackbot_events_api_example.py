0f7ed126b284a4d28a5e86f6d6cf68310f80e9c0
jasonsalas <jason@kuam.com>
2020-04-18 14:19:43 +1000

    updated demo to April 2020 API
    updated the code to play nicely with Slack's official demo https://github.com/slackapi/python-slackclient/tree/master/tutorial
    
    - slight versioning changes to package depencies in requirements.txt
    - changed method calls and SSL certificate
    - added explicit Flask routes (this isn't completely necessary, but it looks nice by convention)
    - outputs logging to the server terminal of traffic

--- slackbot_events_api_example.py
+++ slackbot_events_api_example.py
@@ -1,14 +1,19 @@
 from slackeventsapi import SlackEventAdapter
-from slackclient import SlackClient
+from slack import WebClient
 import json
+import os
+import ssl as ssl_lib
+import certifi
+import logging
 
 
 tokens = {}
 with open('configs.json') as json_data:
     tokens = json.load(json_data)
 
-slack_events_adapter = SlackEventAdapter(tokens.get("slack_signing_secret"), "/slack/events")
-slack_client = SlackClient(tokens.get("slack_bot_token"))
+app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(tokens.get("slack_signing_secret"), "/slack/events", app)
+slack_web_client = WebClient(tokens.get("slack_bot_token"))
 
 
 @slack_events_adapter.on("message")
@@ -16,8 +21,9 @@ def handle_message(event_data):
     message = event_data["event"]
     if message.get("subtype") is None and "BOT TEST" in message.get('text'):
         channel = message["channel"]
-        send_message = "Responding to `BOT TEST` message sent by user <@%s>" % message["user"]
-        slack_client.api_call("chat.postMessage", channel=channel, text=send_message)
+        user = message["user"]
+        send_message = f'Thanks for chatting with me, <@{user}>! If you can, give me a :heart:'
+        slack_web_client.chat_postMessage(channel=channel, text=send_message)
 
 
 @slack_events_adapter.on("error")
@@ -25,4 +31,9 @@ def error_handler(err):
     print("ERROR: " + str(err))
 
 
-slack_events_adapter.start(port=3000)
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
+    app.run(port=3000, debug=True)
